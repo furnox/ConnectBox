6e076ce14bbe12159fc69137eb63122d82a8cfee
Travis Bretton <travis.bretton@keynetics.com>
2016-11-09 16:41:39 -0700

    Added static file middleware for client and for content

--- server/file_server.js
+++ server/file_server.js
@@ -1,21 +1,19 @@
 #!/usr/bin/env node
 
 var connect = require('connect');
-var serveIndex = require('serve-index')
 var serveStatic = require('serve-static')
 var http = require('http');
+var debug = require('debug')('connectbox');
 
 var app = connect();
 
-// respond to root requests
-//app.use('/',
-	//function(request, response, next) {
-		//console.log('Request from ' + request.connection.remoteAddress);
-		//response.end('Hello from Connect!\n');
-	//}
-//);
-
-//app.use(serveStatic('../content', {'index': ['index.html', 'index.htm']}));
-app.use('/content', serveIndex('../content', {'icons': true}));
+app.use(serveStatic('../client', {'index': ['index.html']}));
+app.use('/content',
+	function(request, response, next) {
+		debug('Request from: ' + request.connection.remoteAddress);
+		response.end("[{'name':'tester'}]");
+	}
+);
+app.use(serveStatic('../content', {'dotfiles':'ignore'}));
 
 http.createServer(app).listen(80);
